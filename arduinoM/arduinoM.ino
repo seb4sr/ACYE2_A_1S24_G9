76782be1585560c9a9110d8c86601407a05186c8
NohemiRamirez <ramirezkarina446@gmail.com>
2024-02-21 14:20:04 -0600

    Sensor y menu

--- arduinoM/arduinoM.ino
+++ arduinoM/arduinoM.ino
@@ -1,7 +1,18 @@
 #include <Stepper.h>
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+int trigPin = 7;
+int echoPin = 6;
+int pingTravelTime;
+
+
+int pasosVuelta = 2048;
+bool vuelt = true;
+int i = 0;
+int btnEmergencia = 12;
+
+Stepper motor(pasosVuelta, 8, 10, 9, 11);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -13,9 +24,12 @@ const int btnBackPin = 2;
 
 int estado = 1;
 
-
 void setup() {
   Serial.begin(9600);
+  motor.setSpeed(15);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(btnEmergencia, INPUT_PULLUP);
 
   pinMode(btnUpPin, INPUT_PULLUP);
   pinMode(btnDownPin, INPUT_PULLUP);
@@ -47,6 +61,7 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(">Filtro");
     estado = 2;
+    delay(300);
   } else if (estado == 2 && digitalRead(btnDownPin) == LOW) {
     lcd.clear();
     lcd.setCursor(1, 0);
@@ -54,6 +69,7 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(">Reset");
     estado = 3;
+    delay(300);
   } else if (estado == 3 && digitalRead(btnUpPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -61,6 +77,7 @@ void loop() {
     lcd.setCursor(1, 1);
     lcd.print("Reset");
     estado = 2;
+    delay(300);
   } else if (estado == 2 && digitalRead(btnUpPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -68,90 +85,158 @@ void loop() {
     lcd.setCursor(1, 1);
     lcd.print("Filtro");
     estado = 1;
-  }else if (estado == 1 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 1 && digitalRead(btnSelectPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Contador: ");
     lcd.setCursor(0, 1);
     lcd.print("NUMERO");
     estado = 5;
-  }else if (estado == 2 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 2 && digitalRead(btnSelectPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Color");
     lcd.setCursor(1, 1);
     lcd.print("Size");
     estado = 6;
-  }else if (estado == 6 && digitalRead(btnDownPin) == LOW) {
+    delay(300);
+  } else if (estado == 6 && digitalRead(btnDownPin) == LOW) {
     lcd.clear();
     lcd.setCursor(1, 0);
     lcd.print("Color");
     lcd.setCursor(0, 1);
     lcd.print(">Size");
     estado = 7;
-  }else if (estado == 7 && digitalRead(btnUpPin) == LOW) {
+    delay(300);
+  } else if (estado == 7 && digitalRead(btnUpPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Color");
     lcd.setCursor(0, 1);
     lcd.print("Size");
     estado = 6;
-  }else if (estado == 3 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 3 && digitalRead(btnSelectPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Se Reseteo");
     estado = 9;
-  }else if (estado == 5 && digitalRead(btnBackPin) == LOW) {
+    delay(300);
+  } else if (estado == 5 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Histrorial");
     lcd.setCursor(1, 1);
     lcd.print("Filtro");
     estado = 1;
-  }else if (estado == 6 && digitalRead(btnBackPin) == LOW) {
+    delay(300);
+  } else if (estado == 6 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Filtro");
     lcd.setCursor(1, 1);
     lcd.print("Reset");
     estado = 2;
-  }else if (estado == 7 && digitalRead(btnBackPin) == LOW) {
+    delay(300);
+  } else if (estado == 7 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Filtro");
     lcd.setCursor(1, 1);
     lcd.print("Reset");
     estado = 2;
-  }else if (estado == 9 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 9 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(1, 0);
     lcd.print("Filtro");
     lcd.setCursor(0, 1);
     lcd.print(">Reset");
     estado = 3;
-  }else if (estado == 6 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 6 && digitalRead(btnSelectPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Filtro: Color");
     estado = 10;
-  }else if (estado == 7 && digitalRead(btnSelectPin) == LOW) {
+    delay(300);
+  } else if (estado == 7 && digitalRead(btnSelectPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Filtro: Size");
     estado = 11;
-  }else if (estado == 10 && digitalRead(btnBackPin) == LOW) {
+    delay(300);
+  } else if (estado == 10 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(">Color");
     lcd.setCursor(1, 1);
     lcd.print("Size");
     estado = 6;
-  }else if (estado == 11 && digitalRead(btnBackPin) == LOW) {
+    delay(300);
+  } else if (estado == 11 && digitalRead(btnBackPin) == LOW) {
     lcd.clear();
     lcd.setCursor(1, 0);
     lcd.print("Color");
     lcd.setCursor(0, 1);
     lcd.print(">Size");
     estado = 7;
+    delay(300);
+  }
+
+  if (Sensor()) {
+    while (i < 8) {
+      if (digitalRead(btnEmergencia) == LOW) {
+        Serial.println("Emergencia");
+
+        for (int i = 5; i > 0; i--) {
+          Serial.print(i);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Emergencia");
+          lcd.setCursor(5, 1);
+          lcd.print("Espere ");
+          lcd.print(i);
+          lcd.print("s");
+          delay(1000);
+        }
+        lcd.clear();
+
+        lcd.setCursor(0, 0);
+        lcd.print(">Histrorial");
+        lcd.setCursor(1, 1);
+        lcd.print("Filtro");
+        estado = 1;
+
+        break;
+      } else {
+        motor.step(-pasosVuelta);
+        i += 1;
+      }
+    }
+    delay(5000);
+    Serial.println("se logra");
+    i = 0;
+  }
+}
+
+int Sensor() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pingTravelTime = pulseIn(echoPin, HIGH);
+  Serial.println(pingTravelTime);
+
+  if (pingTravelTime < 520 && pingTravelTime != 0) {
+    Serial.println("Objeto detectado");
+    return true;
+  } else {
+    Serial.println("Ningún objeto detectado");
+    return false;
   }
 }
